33776783b92c203ddf2143a8cc44c23ada06ca85
Andikarya17 <andikaryaputra17@gmail.com>
2024-05-02 15:52:30 +0700

    memasukan node baru di list

--- 0214_doublelinkedlist.cpp
+++ 0214_doublelinkedlist.cpp
@@ -13,9 +13,18 @@ struct Node
 Node* START = NULL;
 
 void addNode() {
-    Node* newNode = new Node();
-    cout << "\nEnter the roll number of the student: ";
+    Node* newNode = new Node(); // step 1: create a new node
+    cout << "\nEnter the roll number of the student: "; // assign value to the data pf the new node
     cin >> newNode->noMhs;
-    cout << "\nEnter the name of the student: ";
+    cout << "\nEnter the name of the student: "; // assign value to the data pf the new node
     cin >> newNode->name; 
 }
+ //insert the new node in the list
+ if (START == NULL || newNode->noMhs <= START->noMhs) {
+    
+    if (START !=NULL && newNode->noMhs == START->noMhs) {
+        cout << "\033[31mDuplicate roll numbers not allowed\033[0m" << endl;
+        return;
+    }
+    
+ }
